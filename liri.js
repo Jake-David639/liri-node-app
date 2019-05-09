37cae76f3dffe29123d15c3e08ba46043d652af4
Jake David <jakedgmu13@gmail.com>
2019-05-09 18:22:40 -0400

    movie-this command working

--- liri.js
+++ liri.js
@@ -9,23 +9,44 @@ const dotenv = require("dotenv").config();
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 
+// taking user cli arguments
+var searchType = process.argv[2];
+var searchTerm = '';
+
 //functions declarations, one to handle each search type
+
+//OMBD API call function, default search term of "Mr. Nobody" is set if no search term is entered.
 movieThis = (searchTerm) => {
-    axios.get("http://www.omdbapi.com/?t=" + searchTerm + "&y=&plot=short&apikey=" + keys.omdb.key).then(
+    if(!searchTerm){
+        searchTerm = 'Mr. Nobody';
+    };
+    axios.get("http://www.omdbapi.com/?t=" + searchTerm + "&plot=short&apikey=" + keys.omdb.key).then(
         function (response) {
-            // Then we print out the imdbRating
-            console.log("The movie's rating is: " + response.data.imdbRating);
+            // print movie info to the terminal
+            // console.log(response.data.Ratings);
+            console.log(`
+    Title: ${response.data.Title}
+    Year released: ${response.data.Released}
+    IMDB rating: ${response.data.imdbRating}
+    Rotten Tomatoes rating: ${response.data.Ratings[1].Value}
+    Filmed in: ${response.data.Country}
+    Languages: ${response.data.Language}
+    Plot Summary: ${response.data.Plot}
+    Lead Actors: ${response.data.Actors}`);
         }
     ).catch(function (err) {
         console.log(err);
     });
 };
 
+
 checkConcerts = (searchTerm) => {
     axios.get("https://rest.bandsintown.com/artists/" + searchTerm + "/events?app_id=" + keys.bandsInTown.key).then(
         function (response) {
             // Then we print out the imdbRating
-            console.log(`${response[0].lineup}/n${response[0].offers}`);
+            console.log(`${response.data[0].lineup}
+            ${response.data[0].offers}`);
+            // console.log(response);
         }
     ).catch(function (err) {
         console.log(err);
@@ -44,25 +65,22 @@ spotifySearch = (searchTerm) => {
 
 };
 
-// taking user cli arguments
-var searchType = process.argv[2];
-var searchTerm = '';
 
 
 // switch for handling each of the search types
 switch (searchType) {
     case 'concert-this':
-        searchTerm = process.argv.slice(3).join(' ');
+        searchTerm = process.argv.slice(3).join('+');
         checkConcerts(searchTerm);
         break;
 
     case 'spotify-this-song':
-        searchTerm = process.argv.slice(3).join(' ');
+        searchTerm = process.argv.slice(3).join('+');
         spotifySearch(searchTerm);
         break;
 
     case 'movie-this':
-        searchTerm = process.argv.slice(3).join(' ');
+        searchTerm = process.argv.slice(3).join('+');
         movieThis(searchTerm);
         break;
 
